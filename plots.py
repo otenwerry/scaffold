de1d42168a68aa4640ab8c7c72fb4282b4e5f5e9
otenwerry <otenwerry@gmail.com>
2025-07-16 17:30:36 -0400

    latin-1 encoding for the dataset

--- plots.py
+++ plots.py
@@ -10,7 +10,7 @@ from scipy.stats import spearmanr, pearsonr
 #read in the training set
 #quoting=3 leaves the quotes alone
 path = "asap-aes/training_set_rel3.tsv"
-df = pd.read_csv(path, sep="\t", quoting=3)
+df = pd.read_csv(path, sep="\t", quoting=3, encoding="latin-1")
 
 #important columns
 ESSAY_COL = "essay"
@@ -19,11 +19,11 @@ SET_COL = "essay_set"
 
 def compute_features(text: str):
     _, bits_per_token = entropy.info_content(text)
-    compressibility = entropy.compressibility(text, 0.9)
-    prop_density, _ = entropy.proposition_density(text)
+    #compressibility = entropy.compressibility(text, 0.9)
+    #prop_density, _ = entropy.proposition_density(text)
     return pd.Series({
-        #"bits_per_token": bits_per_token,
-        "compressibility": compressibility
+        "bits_per_token": bits_per_token
+        #"compressibility": compressibility
         #"prop_density": prop_density #commented out for dependency issues
     })
 
@@ -32,7 +32,7 @@ feature_df= df[ESSAY_COL].progress_apply(compute_features)
 df = pd.concat([df, feature_df], axis=1)
 
 #scatterplots for each feature
-for col in ["compressibility"]: #"bits_per_token", "prop_density"]:
+for col in ["bits_per_token"]: #"compressibility", "prop_density"]:
     g = sns.relplot(
         data=df, x=col, y=SCORE_COL, hue=SET_COL, 
         height=4, aspect=1.2, kind="scatter", alpha=0.4
