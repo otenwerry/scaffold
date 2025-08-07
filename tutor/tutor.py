ace7f95524b8f0b65b87d9bfb87ff553dd25b8d2
otenwerry <otenwerry@gmail.com>
2025-08-06 22:45:51 -0700

    moving loop and pipeline functions into a separate cli file so we can keep testing in cli

--- tutor/tutor.py
+++ tutor/tutor.py
@@ -138,38 +138,3 @@ async def pipeline(png, wav, stdscr):
     await player_task
     stdscr.addstr("\n")
     stdscr.refresh() #update the screen
-
-#until the user presses esc,
-#wait for f9, grab the screen, record the audio, and run the pipeline.
-def loop(stdscr):
-    #enter cbreak mode to make characters immediately available instead of waiting for enter
-    curses.cbreak()
-    #allow special keys like f9 to be detected
-    stdscr.keypad(True)
-    #return -1 if no key is pressed immediately
-    stdscr.nodelay(True)
-    stdscr.addstr("Press F9 to ask.  Esc to quit.\n")
-    while True:
-        #wait for a key to be pressed
-        key = stdscr.getch()
-        if key == 27: #esc
-            break
-        elif key == curses.KEY_F9:
-            stdscr.addstr("Recording... Press F9 to stop.\n")
-            wav_io = record(stdscr)
-            #take a screenshot
-            png = screenshot()
-            #run the pipeline using the wav file and the screenshot
-            asyncio.run(pipeline(png, wav_io, stdscr))
-            #restart the loop
-            stdscr.addstr("Press F9 to ask.  Esc to quit.\n")
-            stdscr.refresh() #update the screen
-        elif key == -1: #no key pressed
-            time.sleep(.02)
-        else: #other key pressed
-            continue
-
-if __name__ == '__main__':
-    #sets up the terminal for curses use, allocates a standard screen object (stdscr),
-    #calls loop(stdscr), and cleans up the terminal when the program exits
-    curses.wrapper(loop)
