503947039f6f546ae61fa73e7a9aa11969cc7878
otenwerry <otenwerry@gmail.com>
2025-08-07 16:53:57 -0700

    accidental indendations

--- tutor/worker.py
+++ tutor/worker.py
@@ -1,4 +1,5 @@
 import asyncio, threading, logging
+import curses
 from core import screenshot, record, pipeline
 
 _log = logging.getLogger("tutor ai")
@@ -8,17 +9,17 @@ class BackgroundWorker:
         self.loop = asyncio.new_event_loop()
         self._t = threading.Thread(target=self.loop.run_forever, daemon=True)
 
-        def start(self):
-            self._t.start()
+    def start(self):
+        self._t.start()
 
-        def stop(self):
-            self.loop.call_soon_threadsafe(self.loop.stop)
-            self._t.join()
+    def stop(self):
+        self.loop.call_soon_threadsafe(self.loop.stop)
+        self._t.join()
 
-        def ask(self):
-            asyncio.run_coroutine_threadsafe(self._job(), self.loop)
-        
-        async def _job(self):
-            png = screenshot()
-            wav = record()
-            await pipeline(png, wav, _log.info)
+    def ask(self):
+        asyncio.run_coroutine_threadsafe(self._job(), self.loop)
+    
+    async def _job(self):
+        png = screenshot()
+        wav = record()
+        await pipeline(png, wav, _log.info)
