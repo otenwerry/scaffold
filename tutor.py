725be1c81a82dc48149d1e7359c1325bd9beddd7
otenwerry <otenwerry@gmail.com>
2025-08-06 20:21:54 -0700

    split loop function into two

--- tutor.py
+++ tutor.py
@@ -10,20 +10,50 @@ from openai import AsyncOpenAI
 #be able to quit while waiting for the model to respond.
 client = AsyncOpenAI()
 
-#screen capture
 def screenshot() -> bytes:
     with mss.mss() as sct:
         img = sct.grab(sct.monitors[0]) #full display
         return mss.tools.to_png(img.rgb, img.size)
 
-#speech to text with whisper
+def record(stdscr, sr=16_000):
+    audio_chunks = []
+    #callback function to continuously record and append to audio_chunks
+    def callback(indata, frames, t, status):
+        audio_chunks.append(indata.copy())
+    #create an audio input stream using the callback function
+    with sd.InputStream(callback=callback, channels=1, samplerate=sr):
+        #run until f9 is released
+        while True:
+            k = stdscr.getch()
+            if k == -1:
+                time.sleep(.02)
+            elif k == curses.KEY_F9:
+                break
+            else:
+                continue
+    stdscr.addstr("Recording done.\n")
+    stdscr.refresh()
+    #concatenate all the audio chunks
+    audio = np.concatenate(audio_chunks)
+    #convert to int16
+    audio_int16 = (audio * 32767).astype(np.int16)
+    #put audio in a wav file
+    wav_io = io.BytesIO()
+    with wave.open(wav_io, 'wb') as wav:
+        wav.setnchannels(1)
+        wav.setsampwidth(2)
+        wav.setframerate(sr)
+        wav.writeframes(audio_int16.tobytes())
+    wav_io.seek(0)
+    wav_io.name = "audio.wav"
+    return wav_io
+
 async def stt(wav_io):
     return (await client.audio.transcriptions.create(
         model='whisper-1', file=wav_io
     )).text
 
-#ask llm a question with a screenshot of the current screen.
-#it "yields" its answer as a stream, rather than waiting for the entire response.
+#llm yields its answer as a stream, rather than waiting for the entire response.
 async def llm(prompt, png_bytes):
     b64_png = base64.b64encode(png_bytes).decode('ascii')
     image_payload = f'data:image/png;base64,{b64_png}'
@@ -46,7 +76,6 @@ async def llm(prompt, png_bytes):
         if delta:
             yield delta
 
-#takes in text, calls the tts model, and puts the response in a queue.
 #queueing allows us to compute audio sentence-by-sentence rather than waiting for the entire response.
 async def tts(text, audio_futures: asyncio.Queue):
     #call API
@@ -57,7 +86,6 @@ async def tts(text, audio_futures: asyncio.Queue):
     task = asyncio.create_task(coro)
     await audio_futures.put(task)
 
-#plays audio from a queue
 async def play(audio_futures: asyncio.Queue):
     while True:
         #get the audio task from the queue (or break if we're done)
@@ -75,69 +103,6 @@ async def play(audio_futures: asyncio.Queue):
         sd.play(audio, fs)
         sd.wait()
         audio_futures.task_done()
-
-
-
-
-
-#until the user presses esc,
-#wait for f9, grab the screen, record the audio, and run the pipeline.
-def loop(stdscr):
-    #enter cbreak mode to make characters immediately available instead of waiting for enter
-    curses.cbreak()
-    #allow special keys like f9 to be detected
-    stdscr.keypad(True)
-    #return -1 if no key is pressed immediately
-    stdscr.nodelay(True)
-    stdscr.addstr("Press F9 to ask.  Esc to quit.\n")
-    while True:
-        #wait for a key to be pressed
-        key = stdscr.getch()
-        if key == 27: #esc
-            break
-        elif key == curses.KEY_F9:
-            stdscr.addstr("Recording... Press F9 to stop.\n")
-            audio_chunks = []
-            #callback function to continuously record and append to audio_chunks
-            def callback(indata, frames, t, status):
-                audio_chunks.append(indata.copy())
-            #create an audio input stream using the callback function
-            with sd.InputStream(callback=callback, channels=1, samplerate=16_000):
-                #run until f9 is released
-                while True:
-                    k = stdscr.getch()
-                    if k == -1:
-                        time.sleep(.02)
-                    elif k == curses.KEY_F9:
-                        break
-                    else:
-                        continue
-            stdscr.addstr("Recording done.\n")
-            #concatenate all the audio chunks
-            audio = np.concatenate(audio_chunks)
-            #convert to int16
-            audio_int16 = (audio * 32767).astype(np.int16)
-            #put audio in a wav file
-            wav_io = io.BytesIO()
-            with wave.open(wav_io, 'wb') as wav:
-                wav.setnchannels(1)
-                wav.setsampwidth(2)
-                wav.setframerate(16_000)
-                wav.writeframes(audio_int16.tobytes())
-            wav_io.seek(0)
-            wav_io.name = "audio.wav"
-            #take a screenshot
-            png = screenshot()
-            #run the pipeline using the wav file and the screenshot
-            asyncio.run(pipeline(png, wav_io, stdscr))
-            #restart the loop
-            stdscr.addstr("Press F9 to ask.  Esc to quit.\n")
-            stdscr.refresh() #update the screen
-        elif key == -1: #no key pressed
-            time.sleep(.02)
-        else: #other key pressed
-            continue
-
 
 #main pipeline: given a screenshot and audio,
 #transcribe the audio, ask the llm, and speak the answer.
@@ -173,6 +138,36 @@ async def pipeline(png, wav, stdscr):
     await player_task
     stdscr.addstr("\n")
     stdscr.refresh() #update the screen
+
+#until the user presses esc,
+#wait for f9, grab the screen, record the audio, and run the pipeline.
+def loop(stdscr):
+    #enter cbreak mode to make characters immediately available instead of waiting for enter
+    curses.cbreak()
+    #allow special keys like f9 to be detected
+    stdscr.keypad(True)
+    #return -1 if no key is pressed immediately
+    stdscr.nodelay(True)
+    stdscr.addstr("Press F9 to ask.  Esc to quit.\n")
+    while True:
+        #wait for a key to be pressed
+        key = stdscr.getch()
+        if key == 27: #esc
+            break
+        elif key == curses.KEY_F9:
+            stdscr.addstr("Recording... Press F9 to stop.\n")
+            wav_io = record(stdscr)
+            #take a screenshot
+            png = screenshot()
+            #run the pipeline using the wav file and the screenshot
+            asyncio.run(pipeline(png, wav_io, stdscr))
+            #restart the loop
+            stdscr.addstr("Press F9 to ask.  Esc to quit.\n")
+            stdscr.refresh() #update the screen
+        elif key == -1: #no key pressed
+            time.sleep(.02)
+        else: #other key pressed
+            continue
 
 if __name__ == '__main__':
     #sets up the terminal for curses use, allocates a standard screen object (stdscr),
