93e2fc46bbeee6dd1e9486000e642f1b231f39a8
Caroline Smyth <caroline.smyth@gmail.com>
2025-08-31 13:01:24 -0400

    dmg

---
+++ web/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="flex flex-row justify-between">
+      <h1 className="text-4xl text-blue-500 font-bold">Login</h1>
+    </div>
+  );
+}
