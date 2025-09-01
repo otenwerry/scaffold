d187c5b1bdd6cc16b945f077e8ed90f3c88bb7d3
Caroline Smyth <caroline.smyth@gmail.com>
2025-08-31 23:35:50 -0400

    header

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,9 +3,9 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="">
       <Header />
-      <main>
+      <main className="mt-50 flex flex-col items-center justify-center">
         <a href="../downloads/Tutor-mac.zip" className="text-blue-500">Download</a>
         <p className="text-sm text-gray-500">First time? After installing, right-click the app and choose "Open".</p>
       </main>
