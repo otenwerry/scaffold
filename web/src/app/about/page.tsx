652110abda42034fb0cfebc19ce6b7b27ef1d0a5
sungjoon0710 <sungjoonpark0710@gmail.com>
2025-10-27 20:49:02 -0400

    style overhaul

--- web/src/app/about/page.tsx
+++ web/src/app/about/page.tsx
@@ -6,12 +6,13 @@ export default function About() {
       <Header />
 
       <div className="wireframe-bg"></div>
-      <div className="wireframe-grid"></div>
-      <div className="wireframe-element"></div>
-      <div className="wireframe-element"></div>
 
       <main className="relative z-10 flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] px-4">
         <h1 className="text-2xl font-bold text-white">Manifesto here</h1>
+        <p className="text-lg md:text-lg text-gray-400 max-w-3xl mx-auto leading-relaxed text-left">
+          Scaffold only works on Macs with Apple chips right now. Compatibility with Intel chips and Windows machines coming later.
+          The app only looks at your screen when you finish asking a question, and doesn’t save any of your data.
+        </p>
 
       </main>
     </div>
--- web/src/app/contact/page.tsx
+++ web/src/app/contact/page.tsx
@@ -6,13 +6,11 @@ export default function Contact() {
       <Header />
 
       <div className="wireframe-bg"></div>
-      <div className="wireframe-grid"></div>
-      <div className="wireframe-element"></div>
-      <div className="wireframe-element"></div>
-      <div className="wireframe-element"></div>
 
       <main className="relative z-10 flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] px-4">
-        <h1 className="text-2xl font-bold text-white">If you run into any issues, please contact us at <a href="mailto:owenkterry@gmail.com">owenkterry@gmail.com</a></h1>
+        <div className="max-w-4xl mx-auto background-white max-h-[800px]">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSduVzouT2-1FGfuQSuQp9IKymfPvfwZDLnVt5yxo5zLm70Kig/viewform?embedded=true" width={800} height={700} frameBorder="0" title="Contact Form" style={{ background: "white" }}>Loading…</iframe>
+        </div>
       </main>
 
     </div>
